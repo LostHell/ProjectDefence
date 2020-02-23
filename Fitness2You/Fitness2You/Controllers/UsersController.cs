5a2abd3356b89bab7e0720fdb20a0db0e89bd74b
E.Karadzhov <losthell92@gmail.com>
2020-02-23 18:47:44 +0100

    Add background img, add index and login pages, add links (/Home/Index,/Users/Login,Users/Register)

---
+++ Fitness2You/Fitness2You/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness2You.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
