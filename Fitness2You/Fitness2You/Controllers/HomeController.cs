f01b675fae5323c994796d39c010a1f6da59b00d
E.Karadzhov <losthell92@gmail.com>
2020-03-23 01:24:51 +0100

    Added Users and Subscriptions Controller refactored _Layout and Login in Views, added new Views Subscriptions

--- Fitness2You/Fitness2You/Controllers/HomeController.cs
+++ Fitness2You/Fitness2You/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Fitness2You.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Fitness2You.Models;
+using System.Diagnostics;
 
 namespace Fitness2You.Controllers
 {
---
+++ Fitness2You/Fitness2You/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fitness2You.Controllers
+{
+    public class UsersController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
